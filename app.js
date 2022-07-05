321bab437d3772beccd0aa05eba8da1a8af16c9e
Charrs-Barwin <mikehollander84@gmail.com>
2022-07-04 21:03:52 -0400

    setup user auth

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Hello World test3"));
+app.get("/", (req, res) => res.send("Hello World te  st4"));
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port `));
-
-
-
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+  
